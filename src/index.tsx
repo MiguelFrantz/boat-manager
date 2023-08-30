780e6cdc282c7c4f6ba48cab6301c1b9d3e4050f
miguel.frantz <frantz.miguel@gmail.com>
2023-08-30 21:18:54 +0200

    fix: adjust file name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './service-worker-registration';
 import reportWebVitals from './reportWebVitals';
 import { enableMock } from './mock';
 
